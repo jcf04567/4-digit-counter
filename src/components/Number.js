b143a4105ddd6beb19b86c3f84dfd0cdca8e88bc
kamiya <78120565+jcf04567@users.noreply.github.com>
2023-09-21 15:46:25 +0900

    ４桁の数字を求められるようにした

--- src/components/Number.js
+++ src/components/Number.js
@@ -2,39 +2,68 @@ import { useState } from "react";
 import CreateButton from "./CreateButton";
 
 const Number = () => {
-  const [countNumber, setCountNumber] = useState();
-  const [number, setNumber] = useState();
+  const [countNumber, setCountNumber] = useState("*");
+  const [number_1, setNumber_1] = useState();
+  const [number_2, setNumber_2] = useState();
+  const [number_3, setNumber_3] = useState();
+  const [number_4, setNumber_4] = useState();
   const [buttonActivity, setButtonActivity] = useState(false);
 
   const setNumberHandler = () => {
     setButtonActivity(true);
-    setNumber();
-    const randomNumber = Math.floor(Math.random()*10);
+    setCountNumber("*");
+    setNumber_1();
+    setNumber_2();
+    setNumber_3();
+    setNumber_4();
+    const randomNumber1 = Math.floor(Math.random() * 10);
+    const randomNumber2 = Math.floor(Math.random() * 10);
+    const randomNumber3 = Math.floor(Math.random() * 10);
+    const randomNumber4 = Math.floor(Math.random() * 10);
     const Interval = setInterval(() => {
-
       setCountNumber((prev) => {
         if (prev === undefined) {
-          prev = 0 ;
+          prev = 0;
         }
-        prev < 9 ? prev += 1 :  prev = 0 ;
+        prev < 9 ? (prev += 1) : (prev = 0);
         return prev;
-      } );
-    },100);
+      });
+    }, 10);
 
     setTimeout(() => {
+      setNumber_1(randomNumber1);
+    }, 3000);
+    setTimeout(() => {
+      setNumber_2(randomNumber2);
+    }, 3500);
+    setTimeout(() => {
+      setNumber_3(randomNumber3);
+    }, 4000);
+    setTimeout(() => {
       clearInterval(Interval);
-      setNumber(randomNumber);
+      setNumber_4(randomNumber4);
       setCountNumber(null);
       setButtonActivity(false);
-    },3000);
-
-  }
+    }, 4500);
+  };
   return (
     <>
       <h3>
-        <p>{countNumber === undefined & number === undefined ? "Get Number!" : ""}</p>
         <span>
-          {countNumber}{number}
+          {number_1 === undefined && countNumber}
+          {number_1}
+        </span>
+        <span>
+          {number_2 === undefined && countNumber}
+          {number_2}
+        </span>
+        <span>
+          {number_3 === undefined && countNumber}
+          {number_3}
+        </span>
+        <span>
+          {number_4 === undefined && countNumber}
+          {number_4}
         </span>
       </h3>
       <CreateButton
